1d3ae2bc07a04f9b79c65c4d7d9f25a7853efb1f
Michael Fehrenbach <mf@michaelfehrenbach.com>
2022-02-05 12:36:18 -0500

    In a function.

--- shuffle.js
+++ shuffle.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+const shuffleClass = () => {
 	const classList = document.querySelector('ul')
 
 	for (index = 0; index < classList.children.length; index += 1) {
 		classList.appendChild(classList.children[Math.random() * index | 0])
 	}
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	shuffleClass()
 })
